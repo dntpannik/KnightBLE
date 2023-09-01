55809dcb8e479d50583f5e7f2475ed8978c5e2ec
Chris <dntpannik@gmail.com>
2023-08-31 23:10:26 -0700

    Updates

---
+++ KnightBLE/Models/KnightType.swift
@@ -0,0 +1,8 @@
+//
+//  KnightType.swift
+//  KnightBLE
+//
+//  Created by Christopher Janover on 8/21/23.
+//
+
+import Foundation
---
+++ KnightBLE/Views/BulkActions/BulkActionsView.swift
@@ -0,0 +1,18 @@
+//
+//  BulkActionsView.swift
+//  KnightBLE
+//
+//  Created by Christopher Janover on 8/21/23.
+//
+
+import SwiftUI
+
+struct BulkActionsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BulkActionsView()
+}
